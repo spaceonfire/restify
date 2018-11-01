aacbe572017bd571cd2096bda39d2356a3a19b47
Constantine Karnaukhov <1119@zolotoykod.ru>
2018-11-01 17:24:36 +0400

    feat(SalePaySystemRest): provide rest api for bitrix pay systems

---
+++ install/bitrix/components/goldencode/restify.sale.paysystem/class.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace goldencode\Bitrix\Restify;
+
+use Bitrix\Main\Loader;
+use Bitrix\Main\Localization\Loc;
+use goldencode\Bitrix\Restify\Executors\SalePaySystemRest;
+
+if (!Loader::includeModule('goldencode.restify')) return false;
+
+Loc::loadLanguageFile(__FILE__);
+
+class RestifySalePaySystemComponent extends RouterComponent {
+	public function executeComponent() {
+		$executor = new SalePaySystemRest($this->arParams);
+		$this->setExecutor($executor);
+		$this->route('GET /', [$this, 'readMany']);
+		$this->route('GET /@id', [$this, 'readOne']);
+		$this->start();
+	}
+}
