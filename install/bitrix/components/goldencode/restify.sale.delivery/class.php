d1d126cee832fa2af67ccb964a9783ce08236208
Constantine Karnaukhov <1119@zolotoykod.ru>
2018-11-01 17:22:34 +0400

    feat(SaleDeliveryServiceRest): provide rest api for bitrix delivery systems

---
+++ install/bitrix/components/goldencode/restify.sale.delivery/class.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace goldencode\Bitrix\Restify;
+
+use Bitrix\Main\Loader;
+use Bitrix\Main\Localization\Loc;
+use goldencode\Bitrix\Restify\Executors\SaleDeliveryServiceRest;
+
+if (!Loader::includeModule('goldencode.restify')) return false;
+
+Loc::loadLanguageFile(__FILE__);
+
+class RestifySaleDeliveryComponent extends RouterComponent {
+	public function executeComponent() {
+		$executor = new SaleDeliveryServiceRest($this->arParams);
+		$this->setExecutor($executor);
+		$this->route('GET /', [$this, 'readMany']);
+		$this->route('GET /@id', [$this, 'readOne']);
+		$this->start();
+	}
+}
---
+++ src/Executors/SaleDeliveryServiceRest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace goldencode\Bitrix\Restify\Executors;
+
+use Emonkak\HttpException\InternalServerErrorHttpException;
+use Emonkak\HttpException\NotFoundHttpException;
+use Exception;
+
+class SaleDeliveryServiceRest {
+	use RestTrait;
+
+	private $entity = 'Bitrix\Sale\Delivery\Services\Table';
+
+	/**
+	 * SaleDeliveryServiceRest constructor
+	 * @param array $options executor options
+	 * @throws \Bitrix\Main\LoaderException
+	 * @throws InternalServerErrorHttpException
+	 * @throws Exception
+	 */
+	public function __construct($options) {
+		$this->loadModules([
+			'sale',
+		]);
+		$this->checkEntity();
+		$this->setSelectFieldsFromEntityClass();
+		$this->setPropertiesFromArray($options);
+		$this->registerBasicTransformHandler();
+		$this->buildSchema();
+	}
+
+	public function readMany() {
+		return $this->readORM();
+	}
+
+	public function readOne($id) {
+		$this->registerOneItemTransformHandler();
+		$this->filter = array_merge($this->filter, [
+			[
+				'LOGIC' => 'OR',
+				['ID' => $id],
+				['CODE' => $id],
+			]
+		]);
+
+		// Get only one item
+		$this->navParams = ['nPageSize' => 1];
+
+		$results = $this->readMany();
+
+		if (!count($results)) {
+			throw new NotFoundHttpException();
+		}
+
+		return $results;
+	}
+}
